84e52d9f8b72932a616c1a0fb7e5f49996db7c60
Keisuke KITA <kei.kita2501@gmail.com>
2013-05-22 16:40:24 +0900

    Write spec for Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,17 @@ describe "StaticPages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
