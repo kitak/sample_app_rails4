cda5c40171a6c60cfeb39e35921ccedf36eb13e3
Keisuke KITA <kei.kita2501@gmail.com>
2013-05-27 17:03:13 +0900

    Filled in the code in Listing 5.36

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,4 +35,19 @@ describe "StaticPages" do
     let(:heading) { 'Contact' }
     let(:page_title) { 'Contact' }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
 end
