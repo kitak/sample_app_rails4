38452098907d914bfa776071e002ffe91779d6f0
Keisuke KITA <kei.kita2501@gmail.com>
2013-05-29 12:32:09 +0900

    specの無効なアドレスの配列に`foo@bar..com`を追加し、テストが落ちることを確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,7 @@ describe User do
 
   describe "when email format is valid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com ]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
