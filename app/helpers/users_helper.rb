65fac6443f095f36d40653cf7d8bbd546f6cc7db
Keisuke KITA <kei.kita2501@gmail.com>
2013-05-31 12:47:13 +0900

    Gravatarのアイコン画像のデフォルトサイズを定数にしました

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
+  DEFAULT_GRAVATAR_IMAGE_SIZE = 80
 
   # Returns the Gravatar(http://gravatar.com/) for the given user.
   def gravatar_for(user, option={})
-    option[:size] ||= 80
+    option[:size] ||= DEFAULT_GRAVATAR_IMAGE_SIZE 
 
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
