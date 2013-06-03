ef048cc1f73bd189d5d7103ccb117153d149cd48
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-03 16:38:17 +0900

    Listring8.34のユーティリティ関数とカスタムマッチャを使って実装とテストを分離した

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,9 +16,7 @@ describe "Authentication" do
     describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email", with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        valid_signin(user)
       end
 
       it { should have_title(user.name) }
@@ -35,7 +33,7 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid')}
+      it { should have_error_message('Invalid')}
 
       describe 'after visiting another page' do
         before { click_link "Home" }
