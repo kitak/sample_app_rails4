b04791ef02adb9c4947096504244f356899a8946
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-06-05 14:20:16 +0900

    just finished 9.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -45,5 +46,10 @@ class UsersController < ApplicationController
 
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe "Authentication" do
   subject { page }
-  let(:sign_in) { 'Sign in' }
-  let(:sign_out) { 'Sign out' }
+  let(:sign_in_text) { 'Sign in' }
+  let(:sign_out_text) { 'Sign out' }
 
   describe 'signin page' do
-    before { visit signin_path } 
+    before { visit signin_path }
 
-    it { should have_content(sign_in) }
-    it { should have_title(sign_in) }
+    it { should have_content(sign_in_text) }
+    it { should have_title(sign_in_text) }
   end
 
   describe "signin" do
     before { visit signin_path }
-    
+
     describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -24,25 +24,25 @@ describe "Authentication" do
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user))}
       it { should have_link('Settings', href: edit_user_path(user))}
-      it { should have_link(sign_out, href: signout_path)}
-      it { should_not have_link(sign_in, href: signin_path)}
+      it { should have_link(sign_out_text, href: signout_path)}
+      it { should_not have_link(sign_in_text, href: signin_path)}
       describe "followed by signout" do
-        before { click_link sign_out }
-        it { should have_link(sign_in) }
+        before { click_link sign_out_text }
+        it { should have_link(sign_in_text) }
       end
-    end  
+    end
 
     describe 'with invalid information' do
-      before { click_button sign_in }
+      before { click_button sign_in_text }
 
-      it { should have_title(sign_in) }
+      it { should have_title(sign_in_text) }
       it { should have_error_message('Invalid')}
 
       describe 'after visiting another page' do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error')}
       end
-    end  
+    end
   end
 
   describe "authorization" do
@@ -59,6 +59,23 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user){ FactoryGirl.create(:user, email: "wrong@example.com") }
+
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_title(full_title("Edit user")) }
+      end
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_path) }
       end
     end
   end
