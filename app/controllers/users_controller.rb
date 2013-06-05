fd7b9fd77e433a0083227dd0c30c3be8597abe21
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-06-05 14:31:30 +0900

    finished 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,10 @@ class UsersController < ApplicationController
   # Before actions 
 
   def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
   def correct_user
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,15 @@ module SessionsHelper
     cookies.delete :remember_token
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
   def current_user=(user)
     @current_user = user
   end
