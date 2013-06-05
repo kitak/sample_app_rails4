ce0ac2f77e5c8a4e88e2d8d3dbfc84cc8d1a4f54
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-05 12:50:08 +0900

    9.2.1まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -37,5 +39,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before actions 
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
 end
