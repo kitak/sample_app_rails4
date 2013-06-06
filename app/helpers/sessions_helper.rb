773affe04f5a1d28e1a62daed09bc577691835ed
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-06 16:38:03 +0900

    アンコメントしてテストが通ることを確認

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
 
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
-    #session.delete(:return_to)
+    session.delete(:return_to)
   end
 
   def store_location
