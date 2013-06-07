d0a32ce54a4933865149f6657bed8983278be002
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-07 18:52:12 +0900

    一定間隔でtextareaの値をみて、count downを更新する

--- app/assets/javascripts/count_down_for_micropost.js
+++ app/assets/javascripts/count_down_for_micropost.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
   var $textarea = $("#micropost_content");
+  var $count_down = $("#count_down");
+  var MAX_LENGTH = 140;
 
   if ($textarea.length === 0) {
     return;
   }
 
-  $textarea.keyup(function () {
-    console.log(140 - $textarea.val().length);
-  });
+  $count_down.text(MAX_LENGTH);
+
+  setTimeout(function () {
+    var quantity = MAX_LENGTH - $textarea.val().length;
+    $count_down.text(quantity);
+
+    if(quantity < 0) {
+      $count_down.css('color', '#f00');
+    } else {
+      $count_down.css('color', '#000');
+    }
+
+    setTimeout(arguments.callee, 100);
+  }, 100);
 });
