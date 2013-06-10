ca52d598ab40db1c4b59c05b4bcc4beb02ca4158
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-10 16:19:09 +0900

    ifの後に空白を入れ、functionの後の空白を除いた

--- app/assets/javascripts/count_down_for_micropost.js
+++ app/assets/javascripts/count_down_for_micropost.js
@@ -1,18 +1,17 @@
-$(function () {
+$(function() {
   var $textarea = $('#micropost_content');
   var $count_down = $('#count_down');
   var MAX_LENGTH = 140;
 
-  var setQuantity = function () {
+  var setQuantity = function() {
     var quantity = MAX_LENGTH - $textarea.val().length;
     $count_down.text(quantity);
 
-    if(quantity < 0) {
+    if (quantity < 0) {
       $count_down.css('color', '#f00');
     } else {
       $count_down.css('color', '#000');
     }
-
   };
 
   setQuantity();
