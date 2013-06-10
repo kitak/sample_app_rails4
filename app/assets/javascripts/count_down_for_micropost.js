51cffd626efe83127cafc16ce36d2fb33165dcc0
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-10 16:41:13 +0900

    count_downをjQueryのプラグイン化

--- app/assets/javascripts/count_down_for_micropost.js
+++ app/assets/javascripts/count_down_for_micropost.js
@@ -1,20 +1,3 @@
 $(function() {
-  var $textarea = $('#micropost_content');
-  var $count_down = $('#count_down');
-  var MAX_LENGTH = 140;
-
-  var setQuantity = function() {
-    var quantity = MAX_LENGTH - $textarea.val().length;
-    $count_down.text(quantity);
-
-    if (quantity < 0) {
-      $count_down.css('color', '#f00');
-    } else {
-      $count_down.css('color', '#000');
-    }
-  };
-
-  setQuantity();
-
-  $textarea.on('keyup change', setQuantity);
+  $('#micropost_content').countDown('#count_down');
 });
---
+++ app/assets/javascripts/jquery.sample_app_rails4.count_down.js
@@ -0,0 +1,22 @@
+(function($) {
+  var DEFAULT_MAX_LENGTH = 140;
+
+  var setQuantity = function($target, max_length) {
+    var $textarea = this;
+    var quantity = max_length - $textarea.val().length;
+    $target.text(quantity);
+
+    if (quantity < 0) {
+      $target.css('color', '#f00');
+    } else {
+      $target.css('color', '#000');
+    }
+  };
+
+  $.fn.countDown = function(target, max_length) {
+    max_length = max_length || DEFAULT_MAX_LENGTH;
+    var handler = setQuantity.bind($(this), $(target), max_length);
+    $(this).on('keyup change', handler);
+    handler();
+  };
+})(jQuery);
