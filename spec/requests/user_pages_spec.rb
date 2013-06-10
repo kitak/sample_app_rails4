d6c3976e04fa836a1bacae5406283bf6ca5bb42d
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-10 10:14:07 +0900

    Add tests for micropost pagination.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -59,18 +59,23 @@ describe "UserPages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+    before { 31.times { FactoryGirl.create(:micropost, user: user) }}
+    after { user.microposts.each(&:destroy) }
 
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
 
-    describe "microposts" do
-      it { should have_content(m1.content) }
-      it { should have_content(m2.content) }
-      it { should have_content(user.microposts.count) }
+    describe "microposts pagination" do
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        user.microposts.paginate(page: 1).each do |micropost|
+          expect(page).to have_selector('li', text: micropost.content)
+        end
+      end
     end
   end
 
