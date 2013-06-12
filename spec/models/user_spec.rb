8017c8b716f32374558d2ac6a79d42c545269c3e
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-12 11:06:36 +0900

    valid, invalidのサンプルをひとつずつ加えてテストが通ることを確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,7 +50,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@bar..com foo@bar.com.]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -60,7 +60,7 @@ describe User do
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn kitak@paperboy.co.jp]
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn kitak@paperboy.co.jp xxx@foo.bar.hoge.moge]
       addresses.each do |valid_address|
         @user.email = valid_address
         expect(@user).to be_valid
