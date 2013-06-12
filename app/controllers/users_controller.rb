f96b005c9b7ef2eca3bf3847f1b36657601c6a7e
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-12 17:25:37 +0900

    xml形式のリクエストがきたら、 atom feedをレスポンスする

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,104 @@
+class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :target_general_user, only: :destroy
+  before_action :no_signed_in_user, only: [:new, :create]
+
+  def index
+    @users = User.paginate(page: params[:page])
+
+    respond_to do |format|
+      format.html
+      format.xml
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before actions 
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def no_signed_in_user
+    redirect_to root_path if signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
+  def target_general_user
+    target_user = User.find(params[:id])
+    redirect_to(root_path) if target_user.admin?
+  end
+end
