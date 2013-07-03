eb9c1dd320f4aaab69b35565229c9313a9b98f02
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-03 12:14:01 +0900

    scmやリポジトリの設定を追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,10 @@
 set :application, "sample_app"
 set :repository,  "https://github.com/kitak/sample_app_rails4.git"
+set :scm, :git
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :deploy_to, "/tmp/sample_app"
+set :rails_env, "production"
 
 role :web, "app001.kitak.pb"
 role :app, "app001.kitak.pb"
