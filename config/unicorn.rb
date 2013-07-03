327ab240148c2820afdfb7b3c0893477052e8dd0
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-03 14:11:32 +0900

    create unicorn conf

---
+++ config/unicorn.rb
@@ -0,0 +1,30 @@
+worker_processes 4
+
+working_directory "/var/www/rails/sample_app"
+
+listen "/var/www/rails/sample_app/shared/run/unicorn.sock", :backlog => 64
+pid "/var/www/rails/sample_app/shared/pids/unicorn.pid"
+listen 8080, :tcp_nopush => true
+
+timeout 30
+
+pid "/var/www/rails/sample_app/shared/pids/unicorn.pid"
+
+stderr_path "/var/www/rails/sample_app/shared/log/unicorn.stderr.log"
+stdout_path "/var/www/rails/sample_app/shared/log/unicorn.stdout.log"
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
