5e8c636803707e0a03bd0d114aee72d8d6cfb4b1
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-03 15:53:37 +0900

    fix path etc...

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,16 +2,14 @@ worker_processes 4
 
 working_directory "/var/www/rails/sample_app/current"
 
-listen "/var/www/rails/sample_app/run/unicorn.sock", :backlog => 64
-pid "/var/www/rails/sample_app/pids/unicorn.pid"
+listen "/var/www/rails/sample_app/current/run/unicorn.sock", :backlog => 64
+pid "/var/www/rails/sample_app/current/pids/unicorn.pid"
 listen 8080, :tcp_nopush => true
 
 timeout 30
 
-pid "/var/www/rails/sample_app/pids/unicorn.pid"
-
-stderr_path "/var/www/rails/sample_app/log/unicorn.stderr.log"
-stdout_path "/var/www/rails/sample_app/log/unicorn.stdout.log"
+stderr_path "/var/www/rails/sample_app/current/log/unicorn.stderr.log"
+stdout_path "/var/www/rails/sample_app/current/log/unicorn.stdout.log"
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
