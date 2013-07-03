ab58f044fac8a099db2f85bd014c919b86465f2c
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-03 12:20:22 +0900

    use_sudo false

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,13 @@ set :application, "sample_app"
 set :repository,  "https://github.com/kitak/sample_app_rails4.git"
 set :scm, :git
 set :branch, "master"
-set :deploy_via, :remote_cache
 set :deploy_to, "/tmp/sample_app"
 set :rails_env, "production"
+set :use_sudo, false 
 
 role :web, "app001.kitak.pb"
 role :app, "app001.kitak.pb"
 role :db,  "app001.kitak.pb", :primary => true # This is where Rails migrations will run
-role :db,  "app001.kitak.pb"
+#role :db,  "app001.kitak.pb" # Slave
 
 set :user, 'app'
-task :show_ls, :roles => [:app] do
-  run 'ls -la ~/.ssh'
-end
