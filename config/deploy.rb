5185c7c1bba21bdfb7d8935e8ce6c772d5e41f8b
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-03 18:21:58 +0900

    /etc/init.d/unicornの起動スクリプトを呼び出すようにした(/etc/sudoersの設定をしたから)

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,65 +27,34 @@ set :user_group, 'app'
 set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
 set :unicorn_config, "#{current_path}/config/unicorn.rb"
 
-def remote_file_exists?(full_path)
-  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip == 'true'
-end
-
-def process_exists?(pid_file)
-  capture("ps -p `cat #{pid_file}`; true").strip.split("\n").size == 2
-end
-
 after "deploy" do
-  run "bundle exec rake db:populate RAILS_ENV=production"
+  run "cd #{current_path}; bundle exec rake db:populate RAILS_ENV=production"
   start
 end
 
 namespace :deploy do
   desc 'Start Unicorn'
   task :start, roles: :app, except: {no_release: true} do
-    if remote_file_exists? unicorn_pid
-      if process_exists? unicorn_pid
-        logger.important 'Unicorn is already running!', 'Unicorn'
-        next
-      else
-        run "rm #{unicorn_pid}"
-      end
-    end
-
     logger.important 'Starting Unicorn...', 'Unicorn'
-    run "cd #{current_path} && bundle exec unicorn -c #{unicorn_config} -E #{rails_env} -D"
+    run "sudo /etc/init.d/unicorn start"
   end
 
   desc 'Stop Unicorn'
   task :stop, :roles => :app, :except => {:no_release => true} do
-    if remote_file_exists? unicorn_pid
-      if process_exists? unicorn_pid
-        logger.important 'Stopping Unicorn...', 'Unicorn'
-        run "kill -s QUIT `cat #{unicorn_pid}`"
-      else
-        run "rm #{unicorn_pid}"
-        logger.important 'Unicorn is not running.', 'Unicorn'
-      end
-    else
-      logger.important 'No PIDs found. Check if unicorn is running.', 'Unicorn'
-    end
+    logger.important 'Stopping Unicorn...', 'Unicorn'
+    run "sudo /etc/init.d/unicorn stop"
   end
 
   desc 'Reload Unicorn'
   task :reload, :roles => :app, :except => {:no_release => true} do
-    if remote_file_exists? unicorn_pid
-      logger.important 'Reloading Unicorn...', 'Unicorn'
-      run "kill -s HUP `cat #{unicorn_pid}`"
-    else
-      logger.important 'No PIDs found. Starting Unicorn...', 'Unicorn'
-      run "cd #{current_path} && bundle exec unicorn -c #{unicorn_config} -E #{rails_env} -D"
-    end
+    logger.important 'Reloading Unicorn...', 'Unicorn'
+    run "sudo /etc/init.d/unicorn reload"
   end
 
   desc 'Restart Unicorn'
   task :restart, :roles => :app, :except => {:no_release => true} do
-    stop
-    start
+    logger.important 'Restarting Unicorn...', 'Unicorn'
+    run "sudo /etc/init.d/unicorn restart"
   end
 end
 
