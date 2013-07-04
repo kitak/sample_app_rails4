b642f6f017a295d2112fc697736530830dd4f387
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-04 10:55:41 +0900

    use dalli

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,9 @@ SampleApp::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store, 'localhost'
+  config.session_store :dalli_store
+  config.session_options = {:cookie_only => false}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
