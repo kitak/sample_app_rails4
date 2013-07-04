8980f5c6997524a772dc3d07d84532347c2756d1
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-04 11:48:23 +0900

    production環境だけmemcachedを使う

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
-SampleApp::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 3.day 
+if Rails.env.production?
+  SampleApp::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 3.day 
+else
+  SampleApp::Application.config.session_store :cookie_store, key: '_sample_app_session' 
+end
