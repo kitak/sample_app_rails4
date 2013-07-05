411acd8937fa752e049aecf9ff3f05b2de0764db
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-05 13:58:17 +0900

    use dalli for session_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.cache_store = :dalli_store
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
-if Rails.env.production?
-  require 'action_dispatch/middleware/session/dalli_store'
-  Rails.application.config.session_store :dalli_store, :memcache_server => '127.0.0.1:11211', :namespace => 'sessions', :key => '_foundation_session', :expire_after => 3.days
-else
-  SampleApp::Application.config.session_store :cookie_store, key: '_sample_app_session' 
-end
+require 'action_dispatch/middleware/session/dalli_store'
+Rails.application.config.session_store :dalli_store, :memcache_server => '127.0.0.1:11211', :namespace => 'sessions', :key => '_foundation_session', :expire_after => 3.days
