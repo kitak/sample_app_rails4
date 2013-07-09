d91f5ecf2a4578e606f8f77396711791353399a2
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-09 16:51:45 +0900

    タスクの移動

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,3 +21,41 @@ def apply_manifest(puppet_role)
   manifest_path = "/home/kitak/sample_app_puppet"
   run "sudo puppet apply --modulepath=#{manifest_path}/modules:#{manifest_path}/roles #{manifest_path}/manifests/#{puppet_role}.pp"
 end
+
+namespace :db do
+  desc 'ダミーデータの生成' 
+  task :populate , roles: :app, execept: {no_release: true} do
+    run "cd #{current_path}; bundle exec rake db:populate RAILS_ENV=production"
+  end
+end
+
+namespace :deploy do
+  desc 'Start Unicorn'
+  task :start, roles: :app, except: {no_release: true} do
+    logger.important 'Starting Unicorn...', 'Unicorn'
+    run "sudo /etc/init.d/unicorn start"
+  end
+
+  desc 'Stop Unicorn'
+  task :stop, :roles => :app, :except => {:no_release => true} do
+    logger.important 'Stopping Unicorn...', 'Unicorn'
+    run "sudo /etc/init.d/unicorn stop"
+  end
+
+  desc 'Reload Unicorn'
+  task :reload, :roles => :app, :except => {:no_release => true} do
+    logger.important 'Reloading Unicorn...', 'Unicorn'
+    run "sudo /etc/init.d/unicorn reload"
+  end
+
+  desc 'Upgrade Unicorn'
+  task :upgrade, :roles => :app, :except => {:no_release => true} do
+    logger.important 'Upgrading Unicorn...', 'Unicorn'
+    run "sudo /etc/init.d/unicorn upgrade"
+  end
+
+  desc "Clear cache"
+  task :clear_cache do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake cache:clear"
+  end
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,44 +18,6 @@ role :app, "app001.kitak.pb", "app002.kitak.pb"
 role :db,  "app001.kitak.pb", "app002.kitak.pb", :primary => true # This is where Rails migrations will run
 set :user, 'app'
 set :user_group, 'app'
-
+g
 set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
 set :unicorn_config, "#{current_path}/config/unicorn.rb"
-
-namespace :db do
-  desc 'ダミーデータの生成' 
-  task :populate , roles: :app, execept: {no_release: true} do
-    run "cd #{current_path}; bundle exec rake db:populate RAILS_ENV=production"
-  end
-end
-
-namespace :deploy do
-  desc 'Start Unicorn'
-  task :start, roles: :app, except: {no_release: true} do
-    logger.important 'Starting Unicorn...', 'Unicorn'
-    run "sudo /etc/init.d/unicorn start"
-  end
-
-  desc 'Stop Unicorn'
-  task :stop, :roles => :app, :except => {:no_release => true} do
-    logger.important 'Stopping Unicorn...', 'Unicorn'
-    run "sudo /etc/init.d/unicorn stop"
-  end
-
-  desc 'Reload Unicorn'
-  task :reload, :roles => :app, :except => {:no_release => true} do
-    logger.important 'Reloading Unicorn...', 'Unicorn'
-    run "sudo /etc/init.d/unicorn reload"
-  end
-
-  desc 'Upgrade Unicorn'
-  task :upgrade, :roles => :app, :except => {:no_release => true} do
-    logger.important 'Upgrading Unicorn...', 'Unicorn'
-    run "sudo /etc/init.d/unicorn upgrade"
-  end
-
-  desc "Clear cache"
-  task :clear_cache do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake cache:clear"
-  end
-end
