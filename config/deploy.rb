6835d32d4cf7b8b920c411cca731c8f9e027caac
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-09 16:48:38 +0900

    デフォルトのstageを設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,43 +2,8 @@ require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
 load 'deploy/assets'
 
-namespace :db do
-  desc 'ダミーデータの生成' 
-  task :populate , roles: :app, execept: {no_release: true} do
-    run "cd #{current_path}; bundle exec rake db:populate RAILS_ENV=production"
-  end
-end
-
-namespace :deploy do
-  desc 'Start Unicorn'
-  task :start, roles: :app, except: {no_release: true} do
-    logger.important 'Starting Unicorn...', 'Unicorn'
-    run "sudo /etc/init.d/unicorn start"
-  end
-
-  desc 'Stop Unicorn'
-  task :stop, :roles => :app, :except => {:no_release => true} do
-    logger.important 'Stopping Unicorn...', 'Unicorn'
-    run "sudo /etc/init.d/unicorn stop"
-  end
-
-  desc 'Reload Unicorn'
-  task :reload, :roles => :app, :except => {:no_release => true} do
-    logger.important 'Reloading Unicorn...', 'Unicorn'
-    run "sudo /etc/init.d/unicorn reload"
-  end
-
-  desc 'Upgrade Unicorn'
-  task :upgrade, :roles => :app, :except => {:no_release => true} do
-    logger.important 'Upgrading Unicorn...', 'Unicorn'
-    run "sudo /etc/init.d/unicorn upgrade"
-  end
-
-  desc "Clear cache"
-  task :clear_cache do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake cache:clear"
-  end
-end
+set :stages, ["app", "db"]
+set :default_stage, "app"
 
 namespace :puppet do
   namespace :apply do
