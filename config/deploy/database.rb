a5d423ba6f080bc6366242db8455da107dcadfe3
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-09 09:29:23 +0900

    `use sudo` false

--- config/deploy/database.rb
+++ config/deploy/database.rb
@@ -3,3 +3,4 @@ role :app, "db001.kitak.pb"
 role :db,  "db001.kitak.pb", :primary => true # This is where Rails migrations will run
 set :user, 'kitak'
 set :user_group, 'paperboy'
+set :use_sudo, false 
